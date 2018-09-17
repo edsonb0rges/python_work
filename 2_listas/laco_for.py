8af1532d2d093cd6119155c9b8683574eb7ca610
edsonb0rges <31133194+edsonb0rges@users.noreply.github.com>
2018-09-17 14:37:12 -0400

    Update laco_for.py

--- 2_listas/laco_for.py
+++ 2_listas/laco_for.py
@@ -5,7 +5,7 @@ for magico in magicos:
     print(magico)
 # O laço "for" executa a mesma ação em todos os itens de uma lista. Observe que há uma diferença entre print(magicos) e print(magico)
 # O laço "for" equivale a dizer: para todo mágico na lista de mágicos, exiba o nome do mágico
-# Como a lista magicos foi armazenada como variável magico, o interpretador irá repetir todo o laço mais de uma vez, até o último elemento da lista. Ao encerrar esse processo, o interpretador passa para o próximo passo do código.
+# Como a lista "magicos" foi armazenada como variável "magico", o interpretador irá repetir todo o loop mais de uma vez, até o último elemento da lista. Ao encerrar esse processo, o interpretador passa para o próximo passo do código.
 # Tenha em mente que o conjunto de passos será repetido, uma vez para cada item da lista, não importa quantos itens haja na lista
 
 
@@ -14,6 +14,6 @@ for magico in magicos:
 
     print(magico.title() + ", esse foi um ótimo truque.")
     print("Eu não vejo a hora de ver um outro truque, " + magico.title() + ".\n")
-    # Perceba que o laço "for" está retornando todo o comando, depois que ele passa para o próximo item (mágico) da lista.
+    # Perceba que o laço "for" está retornando todo o comando e só depois que ele passa para o próximo item "mágico" da lista.
 
-print("Obrigado, pessoal. Foi um grnde show de mágica!")
+print("Obrigado, pessoal. Foi um grande show de mágica!")
