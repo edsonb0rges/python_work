3b5100fbecab9acb1ac8793107022c54878ca295
edsonb0rges <31133194+edsonb0rges@users.noreply.github.com>
2018-09-17 14:38:27 -0400

    Update concatenar.py

--- 1_variaveis_e_tipo_de_dados_simples/concatenar.py
+++ 1_variaveis_e_tipo_de_dados_simples/concatenar.py
@@ -1,4 +1,6 @@
 famous_person = "Albert Einstein"
-citacao = '"Uma pessoa que nunca cometeu um erro jamais tentou nada novo".' #Repare na sequênci de aspas simples e duplas. Use ('.') para adicionar uma (".") dentro dela.
+citacao = '"Uma pessoa que nunca cometeu um erro jamais tentou nada novo".' 
+#Repare na sequência de aspas simples e duplas. Use ('.') para adicionar uma (".") dentro dela.
+
 message = famous_person + " certa vez disse: " + citacao
 print(message)
