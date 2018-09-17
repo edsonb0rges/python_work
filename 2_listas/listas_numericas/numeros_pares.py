5062e24b12127670d737519c2c8d6d910961b3d1
edsonb0rges <31133194+edsonb0rges@users.noreply.github.com>
2018-09-17 14:33:02 -0400

    Update numeros_pares.py

--- 2_listas/listas_numericas/numeros_pares.py
+++ 2_listas/listas_numericas/numeros_pares.py
@@ -1,3 +1,4 @@
 numeros_pares = list(range(2,23,2))
 print(numeros_pares)
 # Listar números pares
+# Na list(range(1º elemento: informa o 1º elemento; 2º elemento: informa o último elemento; 3º elemento: soma 2 a cada novo elemento a partir do 1º elemento))
