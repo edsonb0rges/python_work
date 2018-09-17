63410852a6936bc404caf2f8ce321ae8eeb324ed
edsonb0rges <31133194+edsonb0rges@users.noreply.github.com>
2018-09-17 14:26:16 -0400

    Update name_maiuscula_minuscula.py

--- 1_variaveis_e_tipo_de_dados_simples/name_maiuscula_minuscula.py
+++ 1_variaveis_e_tipo_de_dados_simples/name_maiuscula_minuscula.py
@@ -1,6 +1,6 @@
 name = "edson Borges"
 
-print(name.title()) #Letra inicial de cada palavras ficam Maiúsculas.
+print(name.title()) #Letras iniciais de cada palavra ficam Maiúsculas.
 
 print(name.upper()) #UPPERCASE: todas as letras ficam Maiúsculas.
 
